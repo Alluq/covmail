f227a257096ce94c31ec396d3b10dab3a447c345
Allen <allen.lu645@gmail.com>
2020-10-17 20:45:33 -0400

    Added some functionality

--- popup.js
+++ popup.js
@@ -16,6 +16,6 @@ addLine.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+        {code: 'document.getElementById(":tc") = "Ripperoni"'});
   });
 };
