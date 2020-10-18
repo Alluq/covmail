b3ce50a502ce2f3d070b79a63f3f1d686dc44f8f
Allen <allen.lu645@gmail.com>
2020-10-18 00:38:19 -0400

    Added words and lines and added input logic

--- popup.js
+++ popup.js
@@ -6,11 +6,94 @@
 
 let addLine = document.getElementById('addText');
 
+const potential_fillins = [
+  //Fill in 1
+  [
+    "staying safe",
+    "sheltered in place",
+    "6 feet from the nearest stranger",
+    "stocked with toilet paper for the next 20 years",
+    "eating your daily dose of vitamin gummies",
+    "washing your hands for 20 seconds with warm water and lathered soap",
+    "drinking litres of hand sanitizers with final concentration of 80% ethanol or 75% isopropyl alcohol",
+    "making masks a true fashion statement",
+    "staying positive and testing negative", 
+    "spraying lysol on yourself like perfume",
+    "wiping every countertop available with alcohol wipes",
+    "making the switch from tissues to lysol wipes",
+    "disinfecting your life on a bi-hourly basis",
+    "doing chloe ting workouts every other month to convince yourself you\’re fit",
+    "mindlessly scrolling through tiktok",
+    "learning how to renegade",
+    "baking loaves of banana bread that no one wants",
+    "eating loaves of banana bread all of your friends sent you",
+    "stealing all the flour and yeast from grocery store shelves",
+    "enjoying pantless meetings",
+    "jazzing up your zoom backgrounds",
+    "drinking dalgona coffee every morning",
+    "fermenting sourdough starters under your desk",
+    "spending your life savings on delivery apps",
+    "eating takeout every other day to \“support local\”",
+    "getting scanned in medbay",
+    "hiding in the vents of electrical",
+    "planning extensive vacation plans for 2026",
+    "starting a 12 step korean skincare routine",
+    "not forgetting what it feels like to be loved",
+    "not cutting your bangs on impulse"
+
+  ],
+  //Fill in 2
+  [
+    "unusual",
+    "unprecedented",
+    "extraordinary",
+    "inconceivable",
+    "unfamiliar",
+    "unparalleled",
+    "unmatched",
+    "built different",
+    "~novel~",
+    "unmatched",
+    "desolate",
+    "uncertain",
+    "chaotic",
+    "terrifying",
+    "baleful",
+    "arcadian",
+    "incendiary",
+    "platitudinous",
+    "trying",
+    "challenging",
+    "demanding",
+    "exigent",
+    "heavy",
+    "laborious",
+    "grueling",
+    "grindy",
+    "back-breaking"
+
+  ],
+  //Fill in 3
+  [
+    "nooooo don't kill yourself you're so sexy aha"
+  ]
+]
+
+const messages = [];
+
 addLine.onclick = function(element) {
-  let color = element.target.value;
+  let fillin = [potential_fillins[0][Math.floor(Math.random() * potential_fillins[0].length)],
+                potential_fillins[1][Math.floor(Math.random() * potential_fillins[1].length)],
+                potential_fillins[1][Math.floor(Math.random() * potential_fillins[1].length)]];
+  // for(let i = 0; i < potential_fillins.length; i++){
+  //   fillin.push(potential_fillins[0][Math.floor(Math.random() * potential_fillins[i].length)]);
+  // }
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.getElementsByClassName("Am Al editable LW-avf tS-tW")[0].innerHTML += "THIS WORKS! YAY"'});
+              {code: 'document.getElementsByClassName("Am Al editable LW-avf tS-tW")[0].innerHTML += "I hope you are ' 
+              +  fillin[0]  + " in these " + fillin[1] + ' and ' + fillin[2] + ' times.";'});
+        // {code: 'document.getElementsByClassName("Am Al editable LW-avf tS-tW")[0].innerHTML +=' + 
+        // "I hope you are " + fillin[0] + " in these " + fillin[1] + " and " + fillin[2] + " times.\n"});
   });
 };
